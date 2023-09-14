d476a08883e8bf8762cfdffbd87c1d4bded4bebb
Runneso <maks.safronov.2006@gmail.com>
2023-09-15 00:15:02 +0300

    1.1

--- main.py
+++ main.py
@@ -6,6 +6,14 @@ from library.classes.Color import Color
 from library.classes.Apple import Apple
 from library.classes.Snake import Snake
 
+LAST_POINTS = 0
+LENGTH = 2
+FPS = 10
+WHITE = Color(255, 255, 255)
+RED = Color(255, 0, 0)
+BLACK = Color(0, 0, 0)
+GREEN = Color(0, 0, 255)
+
 
 def distance_between_apple_snake(snake: Snake, apple: Apple) -> None:
     global LENGTH
@@ -26,22 +34,29 @@ def teleport_snake(snake: Snake) -> None:
         snake.x = GAME_WIDTH * GAME_SIZE - 0.5
 
 
+def check_points() -> None:
+    global LAST_POINTS
+    if (LENGTH - 2) % 10 == 0 and LAST_POINTS != (LENGTH - 2):
+        sound_points.play()
+        LAST_POINTS = LENGTH - 2
+
+
 if __name__ == "__main__":
     ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID('mycompany.myproduct.subproduct.version')
     pygame.init()
 
     main_screen = pygame.display.set_mode((GAME_WIDTH * GAME_SIZE, GAME_HEIGHT * GAME_SIZE))
+    font = "fonts/Roboto-Black.ttf"
+    font = pygame.font.Font(font, GAME_SIZE)
+    text = font.render(f"Score: {LENGTH - 2}", True, WHITE())
+    sound_points = "sounds/points.mp3"
+    sound_points = pygame.mixer.Sound(sound_points)
     sound_apple_eating = "sounds/apple_eating.mp3"
     sound_apple_eating = pygame.mixer.Sound(sound_apple_eating)
     game_name = "Snake Game"
     pygame.display.set_caption(game_name)
     game_logo = "images/logo.png"
     pygame.display.set_icon(pygame.image.load(game_logo))
-    LENGTH = 2
-    FPS = 60
-    RED = Color(255, 0, 0)
-    BLACK = Color(0, 0, 0)
-    GREEN = Color(0, 0, 255)
     clock = pygame.time.Clock()
 
     snake = Snake((GAME_SIZE * GAME_WIDTH) // 2, (GAME_SIZE * GAME_HEIGHT) // 2, GAME_SIZE // 5)
@@ -49,30 +64,31 @@ if __name__ == "__main__":
 
     running = True
     while running:
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 running = False
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
+                if event.key == pygame.K_LEFT and not snake.directions[1]:
                     snake.directions = [False, False, False, False]
                     snake.directions[0] = True
-                if event.key == pygame.K_RIGHT:
+                if event.key == pygame.K_RIGHT and not snake.directions[0]:
                     snake.directions = [False, False, False, False]
                     snake.directions[1] = True
-                if event.key == pygame.K_DOWN:
+                if event.key == pygame.K_DOWN and not snake.directions[3]:
                     snake.directions = [False, False, False, False]
                     snake.directions[2] = True
-                if event.key == pygame.K_UP:
+                if event.key == pygame.K_UP and not snake.directions[2]:
                     snake.directions = [False, False, False, False]
                     snake.directions[3] = True
-
+        text = font.render(f"Score: {LENGTH - 2}", True, WHITE())
         main_screen.fill(BLACK())
+        main_screen.blit(text, (GAME_WIDTH * GAME_SIZE / 2 - GAME_WIDTH * 2.5, 0))
         pygame.draw.circle(main_screen, RED(), (apple.get_coordinates()), 15)
         snake.check_directions()
         distance_between_apple_snake(snake, apple)
         teleport_snake(snake)
+        check_points()
         for index in range(LENGTH):
             if snake.directions[0]:
                 main_screen.blit(snake.snake[0 if index == 0 else 1],
@@ -88,4 +104,4 @@ if __name__ == "__main__":
                                  (snake.get_coordinates()[0], snake.get_coordinates()[1] + 40 * index))
 
         pygame.display.update()
-        clock.tick(6)
+        clock.tick(30)
